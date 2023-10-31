40449c5a1be6def53e2c5ac767544fb8a6f030a6
dermatocodista <147107292+dermatocodista@users.noreply.github.com>
2023-10-31 05:19:11 -0300

    working

---
+++ content/main.js
@@ -0,0 +1,9 @@
+const shieldC = document.querySelector('.box');
+function agentHome () 
+{
+	alert(SyntaxError('Talvez eu arrume depois'));
+}
+
+function agentFoam () {
+	alert('Foam page\n\n*Não está pronta');
+}
